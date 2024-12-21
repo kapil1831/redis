98ebe7ac8e14419de7765d86da82c949e1a46273
kapil1831 <kapil1207310meena@gmail.com>
2024-12-21 18:36:07 +0530

    initial client setup

---
+++ client/client.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+
+
+#define PORT 8080
+
+void createServer(){
+
+    // fd of file descriptor
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+}
+
+
+int main(int argc, char *argv[])
+{
+   
+    return 0;
+}
+
+
